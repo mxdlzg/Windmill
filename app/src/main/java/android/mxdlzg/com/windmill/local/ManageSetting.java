447170e87559dfa9da21032b54a7fdc1c48d4564
mxdlzg <mxdlzg@163.com>
2017-04-03 18:53:21 +0800

    添加注释

--- app/src/main/java/android/mxdlzg/com/windmill/local/ManageSetting.java
+++ app/src/main/java/android/mxdlzg/com/windmill/local/ManageSetting.java
@@ -11,6 +11,11 @@ import java.io.File;
  */
 
 public class ManageSetting {
+    /**
+     * 是否存在设置
+     * @param context
+     * @return
+     */
     public static boolean settingExisted(Context context){
         File file = new File("/data/data/"+context.getPackageName()+"shared_prefs", Config.SETTING);
         if (file.exists()){
@@ -20,28 +25,60 @@ public class ManageSetting {
         }
     }
 
+    /**
+     * 添加字符串类型设置
+     * @param context
+     * @param name
+     * @param value
+     */
     public static void addStringSetting(Context context, String name, String value){
         SharedPreferences.Editor editor = context.getSharedPreferences(Config.SETTING,Context.MODE_APPEND).edit();
         editor.putString(name,value);
         editor.apply();
     }
+
+
+    /**
+     * 添加int型设置
+     * @param context
+     * @param name
+     * @param value
+     */
     public static void addIntSetting(Context context,String name,int value){
         SharedPreferences.Editor editor = context.getSharedPreferences(Config.SETTING,Context.MODE_APPEND).edit();
         editor.putInt(name,value);
         editor.apply();
     }
 
+    /**
+     * 添加Long型设置
+     * @param context
+     * @param name
+     * @param value
+     */
     public static void addLongSetting(Context context,String name,Long value){
         SharedPreferences.Editor editor = context.getSharedPreferences(Config.SETTING,Context.MODE_APPEND).edit();
         editor.putLong(name,value);
         editor.apply();
     }
 
+    /**
+     * 获取String设置
+     * @param context
+     * @param name
+     * @return
+     */
     public static String getStringSetting(Context context,String name){
         SharedPreferences sharedPreferences = context.getSharedPreferences(Config.SETTING,Context.MODE_PRIVATE);
         return sharedPreferences.getString(name,"");
     }
 
+    /**
+     * 获取Long设置
+     * @param context
+     * @param name
+     * @return
+     */
     public static Long getLongSetting(Context context,String name){
         SharedPreferences sharedPreferences = context.getSharedPreferences(Config.SETTING,Context.MODE_PRIVATE);
         return sharedPreferences.getLong(name,0);
