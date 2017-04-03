9669a17bfc13b8b8f59f542889f0403bf2a0c0bb
mxdlzg <mxdlzg@163.com>
2017-04-03 19:43:05 +0800

    添加周几判断工具

--- app/src/main/java/android/mxdlzg/com/windmill/util/wdUtil.java
+++ app/src/main/java/android/mxdlzg/com/windmill/util/wdUtil.java
@@ -1,8 +1,58 @@
 package android.mxdlzg.com.windmill.util;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.util.Calendar;
+
 /**
  * Created by 廷江 on 2017/4/3.
  */
 
 public class wdUtil {
+    /**
+     * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
+     */
+    public static int dip2px(Context context, float dpValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dpValue * scale+0.5f);
+    }
+
+    /**
+     * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
+     */
+    public static int px2dip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    /**
+     * 获取今天是周几
+     * @return 周几
+     */
+    @NonNull
+    public static String getWeek(){
+        Calendar cal = Calendar.getInstance();
+        int i = cal.get(Calendar.DAY_OF_WEEK);
+        switch (i) {
+            case 1:
+                return "星期日";
+            case 2:
+                return "星期一";
+            case 3:
+                return "星期二";
+            case 4:
+                return "星期三";
+            case 5:
+                return "星期四";
+            case 6:
+                return "星期五";
+            case 7:
+                return "星期六";
+            default:
+                return "";
+        }
+    }
+
+
 }
