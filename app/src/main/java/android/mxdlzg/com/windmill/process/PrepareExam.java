d331f225d45a12dca0a7e2f6562d46b1b35c7483
mxdlzg <mxdlzg@163.com>
2017-05-01 17:54:00 +0800

    解析Exam表，返回String【】 数组

---
+++ app/src/main/java/android/mxdlzg/com/windmill/process/PrepareExam.java
@@ -0,0 +1,51 @@
+package android.mxdlzg.com.windmill.process;
+
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by 廷江 on 2017/5/1.
+ */
+
+public class PrepareExam {
+    private String[] result;
+
+    /**
+     * @param examHTML exam页面源码
+     * @return 返回一个考试表的数组，一行为一个考试，使用‘；’隔开属性
+     */
+    public String[] getExam(String examHTML){
+        //开始解析
+        Document document = Jsoup.parse(examHTML,"utf-8");
+        Elements tables = document.select("table");
+        //遍历tables
+        for (Element table : tables){
+            //如果找到
+            if (table.child(0).child(0).text().equals("我的考试")){
+                //初始化参数
+                result = new String[table.child(0).childNodeSize()-3];
+                int index = 0;
+                //遍历取每一条考试
+                for (Element tr : table.child(0).children()){
+                    if (tr.hasAttr("style")){
+                        String name = tr.child(1).text();
+                        String time = tr.child(2).text();
+                        String place = tr.child(3).text();
+                        String property = tr.child(4).text();
+                        result[index] = name+";"+time+";"+place+";"+property;
+                        index++;
+                    }
+                }
+                //遍历完毕,结束table循环
+                break;
+            }
+        }
+        return result;
+    }
+}
