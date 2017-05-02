a72a86f3b0d783ddc6796327a2c527323b655d93
mxdlzg <mxdlzg@163.com>
2017-05-02 20:02:52 +0800

    更改处理逻辑，只有swipe可以连接网络进行刷新。启动从文件读取，如果没有则提示没有

--- app/src/main/java/android/mxdlzg/com/windmill/main/ExamActivity.java
+++ app/src/main/java/android/mxdlzg/com/windmill/main/ExamActivity.java
@@ -3,9 +3,11 @@ package android.mxdlzg.com.windmill.main;
 import android.mxdlzg.com.windmill.R;
 import android.mxdlzg.com.windmill.adapter.ExamAdapter;
 import android.mxdlzg.com.windmill.local.ManageExam;
+import android.mxdlzg.com.windmill.local.ManageSetting;
 import android.mxdlzg.com.windmill.net.GetExam;
 import android.mxdlzg.com.windmill.net.GetTemplate;
 import android.mxdlzg.com.windmill.process.PrepareExam;
+import android.mxdlzg.com.windmill.util.wdUtil;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -21,6 +23,8 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.mxdlzg.com.windmill.util.wdUtil.getIncrement;
+
 /**
  * Created by 廷江 on 2017/4/29.
  */
@@ -31,19 +35,29 @@ public class ExamActivity extends AppCompatActivity{
     private ExamAdapter adapter;
     private SwipeRefreshLayout swipeLayout;
     private List<String> exams = new ArrayList<>();
+    List<String> cacheList;
+
+    //config
+    private int currentWeek;
+    private int currentDay;
 
     private Handler rcy_handler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
             PrepareExam prepareExam = new PrepareExam();
+            exams.clear();
             exams = prepareExam.getExam(String.valueOf(msg.obj));
-            adapter.setExams(exams);
-            adapter.notifyDataSetChanged();
-            adapter.notifyItemInserted(0);
-            recyclerView.scrollToPosition(0);
-            ManageExam.cacheExam(ExamActivity.this,exams);
-            swipeLayout.setRefreshing(false);
-            Toast.makeText(ExamActivity.this, "获取完毕", Toast.LENGTH_SHORT).show();
+            if (exams.size()<=0){
+                Toast.makeText(ExamActivity.this, "获取失败", Toast.LENGTH_SHORT).show();
+            }else {
+                adapter.setExams(exams);
+                adapter.notifyDataSetChanged();
+                adapter.notifyItemInserted(0);
+                recyclerView.scrollToPosition(0);
+                ManageExam.cacheExam(ExamActivity.this,exams);
+                swipeLayout.setRefreshing(false);
+                Toast.makeText(ExamActivity.this, "获取完毕", Toast.LENGTH_SHORT).show();
+            }
             return true;
         }
     });
@@ -58,6 +72,9 @@ public class ExamActivity extends AppCompatActivity{
         toolbar.setTitle("Exam");
         toolbar.setTitleTextColor(getColor(R.color.white));
         setSupportActionBar(toolbar);
+
+        //config
+        initSetting();
 
         //swipe
         swipeLayout = (SwipeRefreshLayout) findViewById(R.id.exam_swipeLayout);
@@ -82,38 +99,27 @@ public class ExamActivity extends AppCompatActivity{
             }
         });
 
-        List<String> cacheList = ManageExam.getExamList(this);
-        if (cacheList.size()<=0){
-            //获取
-            Toast.makeText(this, "从网络获取", Toast.LENGTH_SHORT).show();
-            new GetExam(new GetTemplate.SuccessCallback() {
-                @Override
-                public void onSuccess(String result) {
-                    Message message = new Message();
-                    message.obj = result;
-                    rcy_handler.sendMessage(message);
-                }
-            }, new GetTemplate.FailCallback() {
-                @Override
-                public void onFail() {
-
-                }
-            },"http://ems.sit.edu.cn:85/student/main.jsp");
-        }else {
-            Toast.makeText(this, "读取存储", Toast.LENGTH_SHORT).show();
-            exams = cacheList;
-        }
-
-
-
-
         //recyclerView
         recyclerView = (RecyclerView) findViewById(R.id.exam_recyclerView);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
         adapter = new ExamAdapter(exams,this);
+        adapter.setThisWeek(currentWeek);
+        adapter.setThisDay(currentDay);
         recyclerView.setAdapter(adapter);
 
+    }
+
+    public void initSetting(){
+        currentWeek = ManageSetting.getIntSetting(this,"currentWeek")+getIncrement(ManageSetting.getLongSetting(this,"time"),System.currentTimeMillis());
+        currentDay = wdUtil.getWeekInt();
+        cacheList = ManageExam.getExamList(this);
+        exams = cacheList;
+        if (exams.isEmpty()){
+            Toast.makeText(this, "未读取到考试文件", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "从文件读取考试信息", Toast.LENGTH_SHORT).show();
+        }
     }
 }
